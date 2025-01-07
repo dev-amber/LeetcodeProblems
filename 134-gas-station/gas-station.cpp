10e552e5845961f1b5875ab6196faff2b505fd3e
Amber naz <ambernaz1000@gmail.com>
2025-01-07 16:25:56 +0500

    Time: 0 ms (100.00%) | Memory: 112.5 MB (13.61%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,24 +1,21 @@
-#include <vector>
-
 class Solution {
 public:
-    int canCompleteCircuit(const std::vector<int>& gas, const std::vector<int>& cost) {
-        int n = gas.size();
-        int totalTank = 0;
-        int currentTank = 0;
-        int startIndex = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            totalTank += gas[i] - cost[i];
-            currentTank += gas[i] - cost[i];
-            
-            if (currentTank < 0) {
-                startIndex = i + 1; // Next station is the new start point
-                currentTank = 0; // Reset the current tank
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+       int totalGas = 0, totalCost = 0, tank = 0, start = 0;
+
+        for (int i = 0; i < gas.size(); ++i) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+
+            // If the tank is negative, reset the start point
+            if (tank < 0) {
+                start = i + 1; // Move to the next station
+                tank = 0; // Reset the tank
             }
         }
-        
+
         // If total gas is less than total cost, return -1
-        return totalTank >= 0 ? startIndex : -1;
+        return totalGas < totalCost ? -1 : start;  
     }
 };
