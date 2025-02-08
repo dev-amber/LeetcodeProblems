3518f88804339cb56d4f18d6df0a4df0aa7d8047
Amber naz <ambernaz1000@gmail.com>
2025-02-08 11:17:45 +0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (34.60%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-         int n = digits.size();
+      int n = digits.size();
     
-    // Traverse the array from the end (least significant digit) to the beginning
-    for (int i = n - 1; i >= 0; --i) {
+    for (int i = n - 1; i >= 0; i--) {
         if (digits[i] < 9) {
-            // If the current digit is less than 9, simply increment it and return the array
-            digits[i]++;
+            digits[i] += 1;
             return digits;
         }
-        // If the current digit is 9, set it to 0 and continue to the next more significant digit
-        digits[i] = 0;
+        digits[i] = 0;  // Set to 0 and carry to next
     }
     
-    // If we finished the loop, all digits were 9, so we need to add a leading 1
+    // If carry remains, add 1 at the start
     digits.insert(digits.begin(), 1);
-    
-    return digits;
+    return digits;   
     }
 };
