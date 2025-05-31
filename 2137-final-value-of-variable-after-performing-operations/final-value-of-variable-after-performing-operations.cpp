dcb5080b6a9c0c555d7ad8aa944fa1337aea931e
Amber naz <ambernaz1000@gmail.com>
2025-05-31 11:52:10 +0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (95.89%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+    int  X=0;
+    for(string op: operations){
+        if(op.find("++") != string::npos ){
+            X++;
+        }else{
+            X--;
+        }
+    }
+     return X;
+  
+     }
+};
