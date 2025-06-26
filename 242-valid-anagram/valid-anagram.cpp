f8912a987e4ea5114fe722bfd633a5ac6991d275
Amber naz <ambernaz1000@gmail.com>
2025-06-26 10:37:26 +0500

    Time: 3 ms (52.17%) | Memory: 9.7 MB (48.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
-        return false;
-    }
 
-    // Copy the strings to avoid modifying the original ones
-    std::string sorted_s = s;
-    std::string sorted_t = t;
-
-    // Sort both strings
-    std::sort(sorted_s.begin(), sorted_s.end());
-    std::sort(sorted_t.begin(), sorted_t.end());
-
-    // Compare the sorted strings
-    return sorted_s == sorted_t;
+          if(s.size() != t.size()) return false;
+          sort(s.begin(),s.end());
+          sort(t.begin(),t.end());
+            return t== s;
+          
     }
 };
