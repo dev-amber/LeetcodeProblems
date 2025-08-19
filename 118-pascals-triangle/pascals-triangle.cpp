7957f822f2056f32ae4c7dfedaafe3398b0b997f
Amber naz <ambernaz1000@gmail.com>
2025-08-19 11:21:24 +0500

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.29%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-         vector<vector<int>> pascalTriangle;  // To store all rows of Pascal's triangle
-        
-        // Base case: return empty if numRows is 0
-        if (numRows == 0) return pascalTriangle;
-
-        // Start generating each row
-        for (int i = 0; i < numRows; ++i) {
-            vector<int> row(i + 1, 1);  // Create a row with i+1 elements, all initialized to 1
-            
-            // Fill in the inner elements of the row (the non-boundary elements)
-            for (int j = 1; j < i; ++j) {
-                row[j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+        vector<vector<int>> ans;
+        for(int i=0; i< numRows;i++){
+            vector<int> curr(i+1, 1);
+            for(int j=1;j<i;j++){
+                curr[j]= ans[i-1][j]+ans[i-1][j-1];
             }
-            
-            // Add the generated row to the Pascal's triangle
-            pascalTriangle.push_back(row);
+            ans.push_back(curr);
         }
-        
-        return pascalTriangle;
+        return ans;
     }
 };
