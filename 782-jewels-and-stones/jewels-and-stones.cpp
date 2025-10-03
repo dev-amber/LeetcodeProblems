9925a918fa4db1bc7ee07db98765f23551545750
Amber naz <ambernaz1000@gmail.com>
2025-10-03 15:08:34 +0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (91.54%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+     int counter=0;
+     for(int i=0;i< stones.size();i++){
+        for(int j=0; j<jewels.size();j++){
+            if(stones[i]== jewels[j]){
+                counter++;
+                break; // loop se bahir a jaya ga 
+            }
+        }
+     }
+     return counter;
+    }
+   
+};
