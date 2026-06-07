256b6c280146a1708892b25fb2bd5ea27bf49f94
Amber naz <ambernaz1000@gmail.com>
2026-06-07 15:37:14 +0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.48%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
- int f(int n){
+
+// DP with recursive method
+ int f(int n,vector<int>& v){
+    if(v[n]!= -1) return v[n];
     if(n==0) return 0;
     if(n==1) return 1;
-    return (f(n-1)+ f(n-2));
+    v[n]=f(n-1, v)+ f(n-2, v);
+    return v[n];
 
  }
 
     int fib(int n) {
-      return f(n);
+        vector<int> v(n+1, -1);
+      return f(n, v);
     }
 };
