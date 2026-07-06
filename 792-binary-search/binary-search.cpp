ffe761186df73ac8ed0006a856ecf44f788c5339
Amber naz <ambernaz1000@gmail.com>
2026-07-06 13:53:37 +0500

    Time: 0 ms (100.00%) | Memory: 31.4 MB (7.35%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,27 +1,16 @@
-#pragma GCC optimize ("O2")
-
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] == target) {
-                return mid; // Found the target
-            } else if (nums[mid] < target) {
-                left = mid + 1; // Search in the right half
-            } else {
-                right = mid - 1; // Search in the left half
-            }
+        int left=0;
+         int right=nums.size()-1;
+
+        while(left<=right){
+           int mid = (left + right) / 2;
+                    if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) left= mid+1;
+            else right=mid-1;
+
         }
-        
-        return -1; // Target not found
+        return -1;
     }
 };
-
