d7284bec0ccb2d53484df85e85c2f43e4cfed084
Amber naz <ambernaz1000@gmail.com>
2026-08-17 14:14:44 +0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.64%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-      double helper(double x, long n){
-        if( n==0) return 1;
-        if(n <0) return helper(1/x,-n);
+     double helper(double x, long n){
+        if(n==0) return 1;
+        // negative case handle
+        if(n<0) return helper(1/x,-n);
 
-        if(n%2==0) return helper(x*x, n/2);
-        else  return x*helper(x*x,(n-1)/2);
-      }
+        // even and odd 
+        if(n%2==0) return helper(x*x,n/2);
+        else return x*helper(x*x,(n-1)/2);
+     }
     double myPow(double x, int n){
        return helper(x, n);
     }
